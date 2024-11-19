edd76834576b3e88c0260b3e4b08f273a6179b35
ccaioadriano <caiobhadriano@gmail.com>
2024-11-18 22:54:23 -0300

    feat: creating float action button

--- src/components/generics/ActionButton.tsx
+++ src/components/generics/ActionButton.tsx
@@ -1,16 +1,27 @@
-import { StyleProp, StyleSheet, Text, TextStyle, TouchableOpacity, ViewStyle } from "react-native";
+import {
+  StyleProp,
+  Text,
+  TextStyle,
+  TouchableOpacity,
+  ViewStyle,
+} from "react-native";
 
 interface ActionButtonProps {
   title: string;
   onPress: () => void;
   containerStyle?: StyleProp<ViewStyle>;
-  textStyle?: StyleProp<TextStyle>; 
+  textStyle?: StyleProp<TextStyle>;
 }
 
-const ActionButton: React.FC<ActionButtonProps> = ({ title, onPress, containerStyle, textStyle }) => {
+const ActionButton: React.FC<ActionButtonProps> = ({
+  title,
+  onPress,
+  containerStyle,
+  textStyle,
+}) => {
   return (
     <TouchableOpacity
-    style={containerStyle}
+      style={containerStyle}
       onPress={() => {
         onPress;
       }}
---
+++ src/components/generics/FloatActionButton.tsx
@@ -0,0 +1,50 @@
+import {
+  StyleProp,
+  TextStyle,
+  TouchableOpacity,
+  ViewStyle,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+interface FloatActionButtonProps {
+  icon: string;
+  size?: number;
+  iconColor?: string;
+  onPress: () => void;
+  containerStyle?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
+}
+
+const FloatActionButton: React.FC<FloatActionButtonProps> = ({
+  icon,
+  size,
+  onPress,
+  containerStyle,
+  iconColor,
+}) => {
+  return (
+    <TouchableOpacity
+      style={
+        !containerStyle
+          ? {
+              borderWidth: 1,
+              borderColor: "rgba(0,0,0,0.2)",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 70,
+              position: "absolute",
+              bottom: 10,
+              right: 10,
+              height: 70,
+              backgroundColor: "#fff",
+              borderRadius: 100,
+            }
+          : containerStyle
+      }
+    >
+      <Icon name={icon} size={size ? size : 30} color="#01a699" />
+    </TouchableOpacity>
+  );
+};
+
+export default FloatActionButton;
